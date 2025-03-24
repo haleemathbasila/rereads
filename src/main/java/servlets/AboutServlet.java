d58f5bc2599bad14e8c479831ef8e8078e2f7148
haleemathbasila <basilabasheermb@gmail.com>
2025-03-24 12:25:43 +0530

    Initial project upload

--- src/main/java/servlets/AboutServlet.java
+++ src/main/java/servlets/AboutServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,37 +10,80 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bittercode.constant.BookStoreConstants;
+import com.bittercode.constant.db.BooksDBConstants;
+import com.bittercode.model.Book;
 import com.bittercode.model.UserRole;
+import com.bittercode.service.BookService;
+import com.bittercode.service.impl.BookServiceImpl;
 import com.bittercode.util.StoreUtil;
-//Http Servlet extended class for showing the about information
+
 public class AboutServlet extends HttpServlet {
+    BookService bookService = new BookServiceImpl();
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         PrintWriter pw = res.getWriter();
-        res.setContentType("text/html");
-        //If the store is logged in as customer or seller show about info
-        if (StoreUtil.isLoggedIn(UserRole.CUSTOMER, req.getSession())) {
-            RequestDispatcher rd = req.getRequestDispatcher("CustomerHome.html");
-            rd.include(req, res);
-            StoreUtil.setActiveTab(pw, "about");
-            pw.println("<iframe src=\"https://flowcv.me/shashirajraja\" class=\"holds-the-iframe\"\r\n"
-                    + "        title=\"My Personal Website\" width=\"100%\" height=\"100%\"></iframe>");
+        res.setContentType(BookStoreConstants.CONTENT_TYPE_TEXT_HTML);
 
-        } else if (StoreUtil.isLoggedIn(UserRole.SELLER, req.getSession())) {
-            RequestDispatcher rd = req.getRequestDispatcher("SellerHome.html");
-            rd.include(req, res);
-            StoreUtil.setActiveTab(pw, "about");
-            pw.println("<iframe src=\"https://flowcv.me/shashirajraja\" class=\"holds-the-iframe\"\r\n"
-                    + "        title=\"My Personal Website\" width=\"100%\" height=\"100%\"></iframe>");
-
-        } else {
-            //If the user is not logged in, ask to login first
-            //Proceed only if logged in or forword to login page
-            RequestDispatcher rd = req.getRequestDispatcher("login.html");
+        if (!StoreUtil.isLoggedIn(UserRole.CUSTOMER, req.getSession())) {
+            RequestDispatcher rd = req.getRequestDispatcher("CustomerLogin.html");
             rd.include(req, res);
             pw.println("<table class=\"tab\"><tr><td>Please Login First to Continue!!</td></tr></table>");
+            return;
         }
 
-    }
+        String bName = req.getParameter(BooksDBConstants.COLUMN_NAME);
+        RequestDispatcher rd = req.getRequestDispatcher("CustomerHome.html");
+        rd.include(req, res);
+        StoreUtil.setActiveTab(pw, "addbook");
+        pw.println("<div class='container my-2'>");
+        if(bName == null || bName.isBlank()) {
+            //render the add book form;
+            showAddBookForm(pw);
+            return;
+        } //else process the add book
+        
+ 
+        try {
+            String uniqueID = UUID.randomUUID().toString();
+            String bCode = uniqueID;
+            String bAuthor = req.getParameter(BooksDBConstants.COLUMN_AUTHOR);
+            double bPrice = Integer.parseInt(req.getParameter(BooksDBConstants.COLUMN_PRICE));
+            int bQty = Integer.parseInt(req.getParameter(BooksDBConstants.COLUMN_QUANTITY));
 
+            Book book = new Book(bCode, bName, bAuthor, bPrice, bQty);
+            String message = bookService.addBook(book);
+            if ("SUCCESS".equalsIgnoreCase(message)) {
+                pw.println(
+                        "<table class=\"tab\"><tr><td>Book Detail Updated Successfully!<br/>Add More Books</td></tr></table>");
+            } else {
+                pw.println("<table class=\"tab\"><tr><td>Failed to Add Books! Fill up CareFully</td></tr></table>");
+                //rd.include(req, res);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            pw.println("<table class=\"tab\"><tr><td>Failed to Add Books! Fill up CareFully</td></tr></table>");
+        }
+    }
+    
+    private static void showAddBookForm(PrintWriter pw) {
+        String form = "<table class=\"tab my-5\" style=\"width:40%;\">\r\n"
+                + "        <tr>\r\n"
+                + "            <td>\r\n"
+                + "                <form action=\"about\" method=\"post\">\r\n"
+                + "                    <!-- <label for=\"bookCode\">Book Code : </label><input type=\"text\" name=\"barcode\" id=\"bookCode\" placeholder=\"Enter Book Code\" required><br/> -->\r\n"
+                + "                    <label for=\"bookName\">Book Name : </label> <input type=\"text\" name=\"name\" id=\"bookName\" placeholder=\"Enter Book's name\" required><br/>\r\n"
+                + "                    <label for=\"bookAuthor\">Book Author : </label><input type=\"text\" name=\"author\" id=\"bookAuthor\" placeholder=\"Enter Author's Name\" required><br/>\r\n"
+                + "                    <label for=\"bookPrice\">Book Price : </label><input type=\"number\" name=\"price\" placeholder=\"Enter the Price\" required><br/>\r\n"
+                + "                    <label for=\"bookQuantity\">Book Qnty : </label><input type=\"number\" name=\"quantity\" id=\"bookQuantity\" placeholder=\"Enter the quantity\" required><br/>\r\n"
+                + "                    <input class=\"btn btn-success my-2\" type=\"submit\" value=\" Add Book \">\r\n"
+                + "                </form>\r\n"
+                + "            </td>\r\n"
+                + "        </tr>  \r\n"
+                + "        <!-- <tr>\r\n"
+                + "            <td><a href=\"index.html\">Go Back To Home Page</a></td>\r\n"
+                + "        </tr> -->\r\n"
+                + "    </table>";
+        pw.println(form);
+    }
 }
--- src/main/java/servlets/SellerLoginServlet.java
+++ src/main/java/servlets/SellerLoginServlet.java
@@ -31,11 +31,11 @@ public class SellerLoginServlet extends HttpServlet {
                 RequestDispatcher rd = req.getRequestDispatcher("SellerHome.html");
 
                 rd.include(req, res);
-                pw.println("    <div id=\"topmid\"><h1>Welcome to Online <br>Book Store</h1></div>\r\n"
+                pw.println("    <div id=\"topmid\"><h1>Welcome to  <br>ReReads</h1></div>\r\n"
                         + "    <br>\r\n"
                         + "    <table class=\"tab\">\r\n"
                         + "        <tr>\r\n"
-                        + "            <td><p>Welcome "+user.getFirstName()+", Happy Learning !!</p></td>\r\n"
+                        + "            <td><p>Welcome "+user.getFirstName()+", Happy Reading !!</p></td>\r\n"
                         + "        </tr>\r\n"
                         + "    </table>");
             } else {
